c5adccf4f4f60616f15ccbc7a33936e4d1810d08
kodename-kian <kodename.kian@gmail.com>
2024-05-17 21:52:20 +0800

    info modal complete

--- src/About.jsx
+++ src/About.jsx
@@ -6,8 +6,9 @@ const About = () => {
   return (
     <>
       <button
-        className="bg-blue-200 text-black active:bg-blue-500 
-      font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+        className="text-white active:text-reef-400 text-xs font-semibold
+                   px-4 py-2 rounded shadow hover:shadow-lg mr-1 mb-1
+                   border border-gray-300 active:border-reef-400"
         type="button"
         onClick={() => setShowModal(true)}
       >
@@ -18,30 +19,50 @@ const About = () => {
         <>
 
           {/*MODAL BOX WRAPPERS */}
-          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50">
+          <div className="flex justify-center items-center 
+                          overflow-x-hidden overflow-y-auto fixed inset-0 z-50">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               
               {/*MODAL BOX PROPER */}
-              <div className="border border-gray-500 rounded-lg shadow-md relative flex flex-col w-full bg-white">
+              <div className="border border-gray-500 rounded-lg shadow-md relative 
+                              flex flex-col w-full bg-white bg-opacity-95">
                 
                 {/*MODAL HEADER*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                  <div className="text-2xl font-semibold">Wats goin awn? </div>
+                  <div className="text-2xl font-semibold">Wats goin awn? 👁️👄👁️ </div>
                 </div>
 
                 {/*MODAL CONTENT*/}
-                <div>
-                  PLACEHOLDER
+                <div className="text-sm flex flex-col items-center justify-start p-5 border-0 max-w-prose space-y-6">
+                  My friend Katrina has an... interesting vocabulary. <br /> <br />
+                  In an effort to communicate with her better,
+                  this translator uses AI to translate
+                  normal sentences into Katrina-speak: to varying degrees of unhingedness. <br /> <br />
+                  Though it doesn't translate her language perfectly, it does a pretty good job
+                  of capturing the chaos she brings, and so it's close enough I think!
+                  
+                  <div className="w-full">
+                    For the technical details, check out the project on 
+                    <a href="https://github.com/kodename-kian/tina-translator" 
+                       className="text-blue-600 hover:text-blue-800"> GitHub</a>.
+                  </div>
+
+                  <div className="text-xs w-full italic">
+                      A big thank you to Katrina for being supportive of my mini-project. Check out her art and 
+                      give her a follow at <a href="https://www.instagram.com/trinaluise/" 
+                                              className="text-blue-600 hover:text-blue-800">@trina.luise</a>!
+                  </div>
+
                 </div>
 
                 {/*MODAL CLOSER*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    className="text-reef-600 active:text-reef-950 background-transparent font-bold uppercase text-sm"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Yay ok!
+                    Yay noice ok!
                   </button>
                 </div>
 
--- src/App.jsx
+++ src/App.jsx
@@ -21,23 +21,26 @@ function App() {
 
   return (
     <>
-      <div className="bg-cloud bg-cover">
-        <div className="flex flex-col min-h-screen justify-center items-center space-y-8">
+      <div className="bg-cloud bg-cover flex justify-center items-center">
+        <div className="flex flex-col min-h-screen justify-center items-center space-y-8 w-1/3">
           
-          <About />
-          
-          <p className="text-5xl font-bold text-white border-1">
-            The Tina Translator
-          </p>
+
+          <div className="w-full">
+            <About />
+
+            <p className="text-5xl font-bold text-white border-1">
+              The Tina Translator
+            </p>
+          </div>
 
           <textarea id="text" rows="6" maxLength="500"
-                    className="block p-2.5 w-1/3 text-sm rounded-md border border-gray-500 
+                    className="block p-2.5 w-full text-sm rounded-md border border-gray-500 
                           focus:ring-2 focus:ring-reef-950" 
                     placeholder="How would Katrina say..."
                     onInput={e => {setInput(e.target.value)}}> 
           </textarea>
 
-          <button className="w-1/6 min-w-fit py-3 px-6 rounded-md hover:brightness-110 
+          <button className="w-full min-w-fit py-3 px-6 rounded-md hover:brightness-110 
                              bg-gradient-to-r from-reef-400 to-reef-600 text-white font-bold
                              disabled:from-reef-100 disabled:to-reef-100 disabled:text-reef-400"
                   onClick={translate}
@@ -46,7 +49,8 @@ function App() {
           </button>
 
           <div id="response"
-               className="block p-2.5 w-1/3 rounded-md border border-gray-300 text-white font-bold"> 
+               className="block p-2.5 w-full rounded-md border border-gray-300 px-4 py-2
+                          text-white font-semibold shadow mr-1 mb-1">
           </div>
 
         </div>
